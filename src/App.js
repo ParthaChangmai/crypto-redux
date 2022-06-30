c79dff6d35ba1957bdfae336cb5447379d689fe1
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-06-30 09:01:13 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import Navbar from './components/Navbar';
+import './app.css';
+import Homepage from './components/Homepage';
+import Exchanges from './components/Exchanges';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
+
 function App() {
 	return (
-		<div className="App">
-			<div>bryh</div>
+		<div className="app">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<Routes>
+							<Route exact path="/" element={<Homepage />} />
+							<Route exact path="/exchanges" element={<Exchanges />} />
+							<Route
+								exact
+								path="/cryptocurrencies"
+								element={<Cryptocurrencies />}
+							/>
+							<Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+							<Route exact path="/news" element={<News />} />
+						</Routes>
+					</div>
+				</Layout>
+				<div className="footer">
+					<Typography.Title
+						level={5}
+						style={{ color: 'white', textAlign: 'center' }}
+					>
+						CryptoWeb <br />
+						Just a dummy project for learning purposes
+					</Typography.Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/cryptocurrencies">Cryptocurrencies</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
+				</div>
+			</div>
 		</div>
 	);
 }
